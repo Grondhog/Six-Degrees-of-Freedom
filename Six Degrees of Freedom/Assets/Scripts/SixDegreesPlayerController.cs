a7dffe8b7750a32ed0716195147f92fc7873766f
Grondhog <Sgronhovd@gmail.com>
2016-04-02 22:01:35 -0700

    Initial Commit
    Mostly worked out kinks getting movement working in a test environment.
    Current problems include: Space/Crtl key only move the player up/down in
    global space, not relative to the player's rotation. and currently when
    the player collides with any Collider the player's collider spins out of
    control need to find a way to handle collision manually. Rotation is
    working, both based on mouse movement and on Q/E keys rolling the
    player. Translation with W/A/S/D working as well.

---
+++ Six Degrees of Freedom/Assets/Scripts/SixDegreesPlayerController.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class SixDegreesPlayerController : MonoBehaviour {
+
+    private float horizTranslation;
+    private float forwardTranslation;
+    private Vector3 horizVelocity;
+    private Vector3 forwardVelocity;
+    private Vector3 acceleration;
+    private Vector3 rotation;
+    private float cameraRotationX;
+
+    [SerializeField]
+    private Camera cam;
+
+    private float currentCameraRotationX = 0f;
+
+    private Rigidbody rb;
+
+    public float speed = 1f;
+    public float maxSpeed = 5f;
+    public float mouseSensitivity = 3f;
+    public float rollSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        translate();
+
+        translateVertical();
+
+        rotate();
+        
+    }
+
+    private void translate()
+    {
+
+        horizTranslation = Input.GetAxisRaw("Horizontal");
+        forwardTranslation = Input.GetAxisRaw("Vertical");
+
+        horizVelocity = transform.right * horizTranslation;
+        forwardVelocity = transform.forward * forwardTranslation;
+
+       
+            //horizVelocity = new Vector3(Mathf.Cos(currentCameraRotationX) * (transform.right.x * horizTranslation), Mathf.Sin(currentCameraRotationX) * transform.right.x * horizTranslation, transform.right.z * horizTranslation);
+      
+            //forwardVelocity = new Vector3(Mathf.Cos(currentCameraRotationX) * (transform.forward.x * forwardTranslation), Mathf.Cos(currentCameraRotationX) * transform.forward.y * forwardTranslation, transform.forward.z * forwardTranslation);
+        
+        
+
+        //print("forward: " + transform.forward);
+        print("camera: " + currentCameraRotationX);//use this
+
+        acceleration = (horizVelocity + forwardVelocity);
+        if(acceleration != Vector3.zero)
+        {
+            acceleration.y = Mathf.Sin(currentCameraRotationX);
+        }
+        
+        acceleration = acceleration.normalized * speed;
+
+
+        if(rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+
+        rb.AddForce(acceleration);
+
+        
+    }
+
+    private void translateVertical()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * speed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            rb.AddForce(transform.up * -speed);
+        }
+    }
+
+    private void rotate()
+    {
+        float yRot = Input.GetAxisRaw("Mouse X");
+        float xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 rotation = new Vector3(0f, yRot, 0f) * mouseSensitivity;
+
+        //rb.AddTorque(rotation);
+
+        transform.Rotate(rotation);
+
+        cameraRotationX = xRot * mouseSensitivity;
+
+        currentCameraRotationX -= cameraRotationX;
+        if (currentCameraRotationX < -360)
+        {
+            currentCameraRotationX = currentCameraRotationX % -360f;
+        }
+        else if (currentCameraRotationX > 360)
+        {
+
+        }
+
+        if (cam != null)
+        {
+            cam.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0, 0);
+        }
+
+        //TODO works, but kinda funky effects on look rotation
+        if(Input.GetKey(KeyCode.Q))
+        {
+            
+            //rb.AddTorque(transform.forward * rollSpeed);
+            transform.Rotate(new Vector3(0f, 0f, rollSpeed));
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            
+            //rb.AddTorque(transform.forward * -rollSpeed);
+            //rb.MoveRotation(Quaternion.Euler(new Vector3(0f, 0f, -mouseSensitivity)));
+            transform.Rotate(new Vector3(0f, 0f, -rollSpeed));
+        }
+    }
+ 
+}
